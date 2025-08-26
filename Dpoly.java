7690ba56f95cc0db0ad82f8ab608035cb93ab42e
TulikaZeth <tulikaanand05@gmail.com>
2025-08-26 23:37:00 +0530

    u

---
+++ Dpoly.java
@@ -0,0 +1,27 @@
+package Oops.Polymorphism;
+//late binding or dynamic polymorphism
+//runtime polymorphism
+class A1
+{
+    void m1(){
+        System.out.println("Coming from m1 method");
+    }
+}
+class B1 extends A1
+{
+    @Override
+    void m1(){
+        System.out.println("Coming from m2 method");
+    }
+}
+public class Dpoly {
+    public static void main(String[] args) {
+         System.out.println("A");
+         
+         A1 b1 = new B1();
+         b1.m1();
+       
+    
+        
+    }
+}
